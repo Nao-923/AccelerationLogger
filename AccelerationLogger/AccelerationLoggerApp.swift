1db3507fed0293e74c679656d06113110e8d46c9
Nao-923 <fnet.non@outlook.jp>
2025-01-20 15:43:00 +0900

    Initial Commit

---
+++ AccelerationLogger/AccelerationLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelerationLoggerApp.swift
+//  AccelerationLogger
+//
+//  Created by fnet on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct AccelerationLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
